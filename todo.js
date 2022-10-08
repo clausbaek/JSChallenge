b8f0f00d0422937d13836e4d6c13343eda438790
clausbaek <cloud303@naver.com>
2022-10-08 16:00:53 +0900

    finished todo.js

--- todo.js
+++ todo.js
@@ -2,40 +2,30 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
-const TODOS_KEY ="todos"
-const toDoArray =[];
+const TODOS_KEY = "todos"
+let toDoArray = [];
 
 
+function savetoDoArray() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDoArray));
+}
 
 
 function deleteToDo(event) {
-
-    // console.log(event.target.parentElement.innerText);
-    // will show what <li> content of event parent
-    
     const litoRemove = event.target.parentElement;
     litoRemove.remove();
 
-    console.log("li is removed", litoRemove);
-    // function summary : remeove li when event was run
-
-}
-function savetoDoArray(){
-    localStorage.setItem(TODOS_KEY,JSON.stringify(toDoArray));
-    //ex) JSON.stringify([1,2,3])  returns "[1,2,3,4]" / couteroperation is parse
-
-    // save todoarray in local storage / in a string / because you can't save arrays in a localstorage
-    // 
+    toDoArray = toDoArray.filter((todo) => todo.id !== parseInt(litoRemove.id));
+    savetoDoArray();
 }
 
 
 
-function paintToDo(newtoDo) {
-    // console.log(newtoDo) newtoDO = some value in todoInput
+function paintToDo(newtodoObject) {
+
     const newlitag = document.createElement("li");
-    // const newspan1 = newlitag.createElement("span");
-    // console.log(newspan1);
-    //  this code is invalid 
+
+    newlitag.id = newtodoObject.id;
 
     const newspan = document.createElement("span");
     const Xbutton = document.createElement("button");
@@ -43,17 +33,12 @@ function paintToDo(newtoDo) {
     newlitag.appendChild(newspan);
     newlitag.appendChild(Xbutton);
 
-    newspan.innerText = newtoDo;
-    Xbutton.innerText = "❌"
+    newspan.innerText = newtodoObject.text;
+    Xbutton.innerText = "Delete"
     Xbutton.addEventListener("click", deleteToDo);
 
-
-    // console.log(newlitag,"newlitag");
-
     toDoList.appendChild(newlitag);
-    //  summary of the function =
-    //  add newltag <li><span> value<button></button> </span></li> in newtoDo input
-    // and add eventlistner to  button
+
 
 }
 
@@ -61,30 +46,35 @@ function handletoDoSubmit(event) {
 
     event.preventDefault();
     const newtoDo = toDoInput.value;
-    // console.log(toDoInput.value)  some event value
+
     toDoInput.value = "";
-    toDoArray.push(newtoDo);
+
+    const newtodoObject = {
+        text: newtoDo,
+        id: Date.now()
+    };
+
+    toDoArray.push(newtodoObject);
+
     savetoDoArray();
 
-    // console.log(toDoForm.value);  value reset to  = ""
-    paintToDo(newtoDo);
+    paintToDo(newtodoObject);
 }
 
 toDoForm.addEventListener("submit", handletoDoSubmit);
 
 
-// const savedtodos =localStorage.getItem(TODOS_KEY);
-// function sayItemType(item) {
-//     console.log("This item is ", item)
+const savedtodos = localStorage.getItem(TODOS_KEY);
 
-// }  * if arrow function is not used
 
-if (savedtodos!==null){
-    const parsedtodos =JSON.parse(savedtodos);
-    // parsedtodos.forEach(sayItemType);
-    // foreach element run sayItemType function
-    //  instead use arrow function for short(instead of function) :
-    
-    parsedtodos.foreach(item) => console.log("this item is ", item);
+if (savedtodos !== null) {
+    const parsedtodos = JSON.parse(savedtodos);
 
-    };
+    toDoArray = parsedtodos;
+    toDoArray.forEach(paintToDo);
+
+    parsedtodos.forEach((item) => console.log("this item is ", item));
+}
+
+
+
